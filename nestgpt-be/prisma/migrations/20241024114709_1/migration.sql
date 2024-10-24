7891a979db7763668cdf99f5608fb1d2f7e1024f
alief abdillah <aliefmabdillah09@gmail.com>
2024-10-24 18:59:19 +0700

    feat: add migration

---
+++ nestgpt-be/prisma/migrations/20241024114709_1/migration.sql
@@ -0,0 +1,15 @@
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_chatId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_senderId_fkey";
+
+-- AlterTable
+ALTER TABLE "Message" ALTER COLUMN "senderId" SET DATA TYPE TEXT,
+ALTER COLUMN "chatId" SET DATA TYPE TEXT;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
