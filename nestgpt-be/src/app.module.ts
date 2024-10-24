1304d0e56e39396d2fffbac914913edbd1a0f4d2
alief abdillah <aliefmabdillah09@gmail.com>
2024-10-24 16:53:19 +0700

    fix: module import

--- nestgpt-be/src/app.module.ts
+++ nestgpt-be/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './module/auth/auth.module';
 import { UserModule } from './module/user/user.module';
 import { AuthModule } from './module/auth/auth.module';
 
