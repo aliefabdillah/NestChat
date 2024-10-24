5ff72d02856243d266478970ce388d5dafac82c2
alief abdillah <aliefmabdillah09@gmail.com>
2024-10-24 17:40:04 +0700

    feat: create login user endpoint

--- nestgpt-be/src/module/auth/auth.controller.ts
+++ nestgpt-be/src/module/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +17,11 @@ export class AuthController {
   @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
   async register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
+  }
+
+  @Post('login')
+  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 }
--- nestgpt-be/src/module/auth/auth.service.ts
+++ nestgpt-be/src/module/auth/auth.service.ts
@@ -6,14 +6,19 @@ import {
 import { UserService } from '../user/user.service';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, password: string) {
     const user = await this.userService.findOne(username);
-    if (user && user.password === password) {
+    if (user && bcrypt.compare(user.password, password)) {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...result } = user;
       return result;
@@ -38,5 +43,21 @@ export class AuthService {
       throw new UnprocessableEntityException();
     }
     return newUser;
+  }
+
+  async login(userLoginDto: LoginDto) {
+    const { username, password } = userLoginDto;
+
+    // check user in database by username and password
+    const user = await this.validateUser(username, password);
+    if (!user) {
+      throw new BadRequestException('Invalid username or Password');
+    }
+
+    // create jwt token
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
