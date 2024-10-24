04fe0fd6bc54d57a6225aa9747f0f712592b1606
alief abdillah <aliefmabdillah09@gmail.com>
2024-10-24 18:17:28 +0700

    feat: get profile endpoint

--- nestgpt-be/src/module/user/user.controller.ts
+++ nestgpt-be/src/module/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/me')
+  async getProfile(@Req() req) {
+    return this.userService.findById(req.user.id);
+  }
+}
--- nestgpt-be/src/module/user/user.service.ts
+++ nestgpt-be/src/module/user/user.service.ts
@@ -1,16 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class UserService {
   constructor(private prismaService: PrismaService) {}
 
+  async findById(id: string) {
+    const userData = await this.prismaService.user.findUnique({
+      where: { id },
+    });
+    if (!userData) {
+      throw new NotFoundException('User not found');
+    }
+
+    return { username: userData.username };
+  }
   async findOne(username: string) {
-    return this.prismaService.user.findFirst({ where: { username } });
+    return await this.prismaService.user.findFirst({ where: { username } });
   }
 
   async create(username: string, password: string) {
-    return this.prismaService.user.create({
+    return await this.prismaService.user.create({
       data: { username, password },
     });
   }
