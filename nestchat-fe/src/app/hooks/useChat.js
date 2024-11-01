9183faa925a73376ced169ff38539b778bfd95ce
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-01 18:00:00 +0700

    feat: add notification state

--- nestchat-fe/src/app/hooks/useChat.js
+++ nestchat-fe/src/app/hooks/useChat.js
@@ -1,35 +1,39 @@
 import { useEffect, useState } from 'react';
- import io from 'socket.io-client';
+import io from 'socket.io-client';
 
- const useChat = (chatId) => {
-   const [messages, setMessages] = useState([]);
-   const [message, setMessage] = useState('');
-   const [socket, setSocket] = useState(null);
+const useChat = (chatId, onNewMessage) => {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  const [socket, setSocket] = useState(null);
 
-   useEffect(() => {
-     const newSocket = io(process.env.SOCKET_URL);
-     setSocket(newSocket);
+  useEffect(() => {
+    const newSocket = io(process.env.SOCKET_URL);
+    setSocket(newSocket);
 
-     newSocket.emit('joinChat', chatId);
+    newSocket.emit('joinChat', chatId);
 
-     newSocket.on('receiveMessage', (newChat) => {
-       setMessages(newChat.messages);
-     });
+    newSocket.on('receiveMessage', (newChat) => {
+      setMessages(newChat.messages);
+      if (onNewMessage) {
+        const newMessage = newChat.messages[newChat.messages.length - 1];
+        onNewMessage(newMessage);
+      }
+    });
 
-     return () => newSocket.close();
-   }, [chatId]);
+    return () => newSocket.close();
+  }, [chatId]);
 
-   const sendMessage = (userId, content) => {
-     socket.emit('sendMessage', { chatId, userId, content });
-     setMessage('');
-   };
+  const sendMessage = (userId, content) => {
+    socket.emit('sendMessage', { chatId, userId, content });
+    setMessage('');
+  };
 
-   return {
-     messages,
-     message,
-     setMessage,
-     sendMessage,
-   };
- };
+  return {
+    messages,
+    message,
+    setMessage,
+    sendMessage,
+  };
+};
 
- export default useChat;
+export default useChat;
